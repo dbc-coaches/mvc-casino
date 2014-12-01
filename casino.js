aa599fe993fb7addb837048acedbf3921d477e1c
Patrick Reynolds <reynolds@adobe.com>
2014-11-30 16:42:31 -0800

    Provide additional detail regarding event binding implementation.

--- casino.js
+++ casino.js
@@ -1,6 +1,6 @@
-var Casino = Casino || {}; // Namespacing to Casino object
+var Casino = Casino || {} // Namespacing to Casino object
 
-(function($, Casino) {
+;(function($, Casino) {
     // Shortcut for: $(document).ready(function() {})
     $(function() {
         Casino.Dealer.start()  // 'Kicks off the game'
@@ -95,21 +95,26 @@ var Casino = Casino || {}; // Namespacing to Casino object
         },
         bindEventListeners: function() {
             // Two options for managing scope
-            // 1. self, _this, that (as implemented)
-            // 2. Bind
+            // 1. self, _this, that
+            // 2. Bind (as implemented)
 
-            var self = this
-            $('.add').on('click', function() {
-                self.addDie()
-            })
+            // var self = this
+            // $('.add').on('click', function() {
+            //     self.addDie()
+            // })
 
-            $('.roll').on('click', function() {
-                self.rollDice()
-            })
+            // $('.roll').on('click', function() {
+            //     self.rollDice()
+            // })
 
-            $('.clear').on('click', function() {
-                self.clearDice()
-            })
+            // $('.clear').on('click', function() {
+            //     self.clearDice()
+            // })
+
+            // OR 2. Bind
+            $('.add').on('click', this.addDie.bind(this))
+            $('.roll').on('click', this.rollDice.bind(this))
+            $('.clear').on('click', this.clearDice.bind(this))
         }
     }
 
